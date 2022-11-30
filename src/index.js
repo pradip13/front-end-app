1fed9f3df199a677bbe69ee1997b7829f2c81e30
pradip <hazarikapradip133@gmail.com>
2022-11-30 13:51:29 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 // use HashRouter to avoid 404 pages when it is deployed to gh-pages
 // import { HashRouter as Router } from 'react-router-dom';
